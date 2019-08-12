5108fd7e0fe0adf2e5ce100e19ca3d5822f32374
mulx10 <mehulkumarnirala@iitkgp.ac.in>
2019-08-12 23:32:47 +0530

    Corrected Indentations

--- VGG19/VGG19_impl.hpp
+++ VGG19/VGG19_impl.hpp
@@ -31,7 +31,7 @@ VGG19::VGG19(const size_t inputWidth,
              weights(weights),
              outputShape(512)
 {
-    VGGNet = new Sequential<>();
+  VGGNet = new Sequential<>();
 }
 
 VGG19::~VGG19()
@@ -41,148 +41,148 @@ VGG19::~VGG19()
 
 Sequential<>* VGG19::CompileModel()
 {
-    // Block 1
-    VGGNet->Add<Convolution<> >(
-      inputChannel,  // Number of input activation maps.
-      64,  // Number of output activation maps.
-      3,  // Filter width.
-      3,  // Filter height.
-      1,  // Stride along width.
-      1,  // Stride along height.
-      1,  // Padding width.
-      1,  // Padding height.
-      inputWidth, // Input width.
-      inputHeight  // Input height.
+  // Block 1
+  VGGNet->Add<Convolution<> >(
+    inputChannel,  // Number of input activation maps.
+    64,  // Number of output activation maps.
+    3,  // Filter width.
+    3,  // Filter height.
+    1,  // Stride along width.
+    1,  // Stride along height.
+    1,  // Padding width.
+    1,  // Padding height.
+    inputWidth, // Input width.
+    inputHeight  // Input height.
+  );
+  VGGNet->Add<LeakyReLU<> >();
+
+  VGGNet->Add<Convolution<> >(64, 64, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+
+  // Add first pooling layer. Pools over 2x2 fields in the input.
+  VGGNet->Add<MaxPooling<> >(
+    2,  // Width of field.
+    2,  // Height of field.
+    2,  // Stride along width.
+    2,  // Stride along height.
+    true
     );
-    VGGNet->Add<LeakyReLU<> >();
 
-    VGGNet->Add<Convolution<> >(64, 64, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
+  std::cout << "inputWidth" << " " << "inputHeight" << std::endl;
 
-    // Add first pooling layer. Pools over 2x2 fields in the input.
-    VGGNet->Add<MaxPooling<> >(
-        2,  // Width of field.
-        2,  // Height of field.
-        2,  // Stride along width.
-        2,  // Stride along height.
-        true
-      );
+  // same padding
+  inputWidth = std::ceil(inputWidth / 2.0);
+  inputHeight = std::ceil(inputHeight / 2.0);
+  std::cout << inputWidth << " " << inputHeight << std::endl;
 
-    std::cout << "inputWidth" << " " << "inputHeight" << std::endl;
+  // Block 2
+  VGGNet->Add<Convolution<> >(64, 128, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<Convolution<> >(128, 128, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<MaxPooling<> >(2, 2, 2, 2, true);
 
-    // same padding
-    inputWidth = std::ceil(inputWidth / 2.0);
-    inputHeight = std::ceil(inputHeight / 2.0);
-    std::cout << inputWidth << " " << inputHeight << std::endl;
+  // same padding
+  inputWidth = std::ceil(inputWidth / 2.0);
+  inputHeight = std::ceil(inputHeight / 2.0);
+  std::cout << inputWidth << " " << inputHeight << std::endl;
 
-    // Block 2
-    VGGNet->Add<Convolution<> >(64, 128, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<Convolution<> >(128, 128, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<MaxPooling<> >(2, 2, 2, 2, true);
+  // Block 3
+  VGGNet->Add<Convolution<> >(128, 256, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<Convolution<> >(256, 256, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<Convolution<> >(256, 256, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<Convolution<> >(256, 256, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<MaxPooling<> >(2, 2, 2, 2, true);
 
-    // same padding
-    inputWidth = std::ceil(inputWidth / 2.0);
-    inputHeight = std::ceil(inputHeight / 2.0);
-    std::cout << inputWidth << " " << inputHeight << std::endl;
+  // same padding
+  inputWidth = std::ceil(inputWidth / 2.0);
+  inputHeight = std::ceil(inputHeight / 2.0);
+  std::cout << inputWidth << " " << inputHeight << std::endl;
 
-    // Block 3
-    VGGNet->Add<Convolution<> >(128, 256, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<Convolution<> >(256, 256, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<Convolution<> >(256, 256, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<Convolution<> >(256, 256, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<MaxPooling<> >(2, 2, 2, 2, true);
+  // Block 4
+  VGGNet->Add<Convolution<> >(256, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<MaxPooling<> >(2, 2, 2, 2, true);
 
-    // same padding
-    inputWidth = std::ceil(inputWidth / 2.0);
-    inputHeight = std::ceil(inputHeight / 2.0);
-    std::cout << inputWidth << " " << inputHeight << std::endl;
+  // same padding
+  inputWidth = std::ceil(inputWidth / 2.0);
+  inputHeight = std::ceil(inputHeight / 2.0);
+  std::cout << inputWidth << " " << inputHeight << std::endl;
 
-    // Block 4
-    VGGNet->Add<Convolution<> >(256, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<MaxPooling<> >(2, 2, 2, 2, true);
+  // Block 5
+  VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
+  VGGNet->Add<LeakyReLU<> >();
+  VGGNet->Add<MaxPooling<> >(2, 2, 2, 2, true);
 
-    // same padding
-    inputWidth = std::ceil(inputWidth / 2.0);
-    inputHeight = std::ceil(inputHeight / 2.0);
-    std::cout << inputWidth << " " << inputHeight << std::endl;
+  // same padding
+  inputWidth = std::ceil(inputWidth / 2.0);
+  inputHeight = std::ceil(inputHeight / 2.0);
+  std::cout << inputWidth << " " << inputHeight << std::endl;
 
-    // Block 5
-    VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<Convolution<> >(512, 512, 3, 3, 1, 1, 1, 1, inputHeight, inputWidth);
-    VGGNet->Add<LeakyReLU<> >();
-    VGGNet->Add<MaxPooling<> >(2, 2, 2, 2, true);
-
-    // same padding
-    inputWidth = std::ceil(inputWidth / 2.0);
-    inputHeight = std::ceil(inputHeight / 2.0);
-    std::cout << inputWidth << " " << inputHeight << std::endl;
-
-    if (includeTop)
+  if (includeTop)
+  {
+    // Add the final dense layer.
+    VGGNet->Add<Linear<> >(inputWidth * inputHeight * 512, 4096);
+    VGGNet->Add<Linear<> >(4096, 4096);
+    VGGNet->Add<Linear<> >(4096, numClasses);
+    outputShape = numClasses;
+  }
+  else
+  {   
+    outputShape = inputWidth * inputHeight * 512;
+    if (pooling == "max")
     {
-        // Add the final dense layer.
-        VGGNet->Add<Linear<> >(inputWidth * inputHeight * 512, 4096);
-        VGGNet->Add<Linear<> >(4096, 4096);
-        VGGNet->Add<Linear<> >(4096, numClasses);
-        outputShape = numClasses;
+      // Global max pooling.
+      VGGNet->Add<MaxPooling<> >(inputWidth, inputHeight, 1, 1, true);
+      outputShape = 512; // 1 * 1 * 512 .
     }
-    else
-    {   
-        outputShape = inputWidth * inputHeight * 512;
-        if (pooling == "max")
-        {
-            // Global max pooling.
-            VGGNet->Add<MaxPooling<> >(inputWidth, inputHeight, 1, 1, true);
-            outputShape = 512; // 1 * 1 * 512 .
-        }
-        else if (pooling == "avg")
-        {
-            // Global avg pooling.
-            VGGNet->Add<MeanPooling<> >(inputWidth, inputHeight, 1, 1, true);
-            outputShape = 512; // 1 * 1 * 512 .
-        }
+    else if (pooling == "avg")
+    {
+      // Global avg pooling.
+      VGGNet->Add<MeanPooling<> >(inputWidth, inputHeight, 1, 1, true);
+      outputShape = 512; // 1 * 1 * 512 .
     }
-    return VGGNet;
+  }
+  return VGGNet;
 }
 
 size_t VGG19::GetOutputShape()
 {
-    return outputShape;
+  return outputShape;
 }
 
 Sequential<>* VGG19::LoadModel(const std::string& filePath)
 {
-    std::cout << "Loading model ..." << std::endl;
-    data::Load(filePath, "VGG19", VGGNet);
-    return VGGNet;
+  std::cout << "Loading model ..." << std::endl;
+  data::Load(filePath, "VGG19", VGGNet);
+  return VGGNet;
 }
 
 void VGG19::SaveModel(const std::string& filePath)
 {
-    std::cout << "Saving model ..." << std::endl;
-    data::Save(filePath, "VGG19", VGGNet);
-    std::cout << "Model saved in " << filePath << std::endl;
+  std::cout << "Saving model ..." << std::endl;
+  data::Save(filePath, "VGG19", VGGNet);
+  std::cout << "Model saved in " << filePath << std::endl;
 }
 
 Sequential<>* VGG19::GetModel()
 {
-    return VGGNet;
+  return VGGNet;
 }
 #endif
--- VGG19/vgg19_tiny_imagenet/src/dataloader.hpp
+++ VGG19/vgg19_tiny_imagenet/src/dataloader.hpp
@@ -33,58 +33,58 @@ namespace fs = boost::filesystem;
 class Dataloader
 {
  public:
- 	/**
- 	* Dataloader constructor.
- 	*
- 	* @param numClasses Number of classes in the dataset.
- 	*/
-	Dataloader(size_t numClasses);
+  /**
+  * Dataloader constructor.
+  *
+  * @param numClasses Number of classes in the dataset.
+  */
+  Dataloader(size_t numClasses);
 
-	~Dataloader();
+  ~Dataloader();
 
-	// To store the name of class labels.
-	std::map<std::string, size_t> targetName;
+  // To store the name of class labels.
+  std::map<std::string, size_t> targetName;
 
-	// To store filenames of training and validation images.
-	std::vector<std::string> trainX, valX;
+  // To store filenames of training and validation images.
+  std::vector<std::string> trainX, valX;
 
-	// To store labels of training and validation images.
-	std::vector<size_t> trainY, valY;
+  // To store labels of training and validation images.
+  std::vector<size_t> trainY, valY;
 
-	// Number of classes in the dataset.
-	size_t numClasses;
+  // Number of classes in the dataset.
+  size_t numClasses;
 
-	/**
-	* Loads the name of training files from the tiny imagenet dataset.
-	*
-	* @param folderPath Path of the training images.
-	* @param shuffle Randomly shuffle the training data.
-	*/
-	void LoadTrainData(const std::string folderPath, bool shuffle = true);
+  /**
+  * Loads the name of training files from the tiny imagenet dataset.
+  *
+  * @param folderPath Path of the training images.
+  * @param shuffle Randomly shuffle the training data.
+  */
+  void LoadTrainData(const std::string folderPath, bool shuffle = true);
 
-	/**
-	* Loads the name of validation files from the tiny imagenet dataset.
-	*
-	* @param folderPath Path of the training images.
-	*/
-	void LoadValData(const std::string folderPath);
-	
-	/**
-	* Loads images into armadillo matrix.
-	*
-	* @param X Matrix where images are loaded.
-	* @param y Labels associated with the images
-	* @param train Boolean variable to identify the dataset type (i.e train/val).
-	* @param limit Number of datapoints to be stored (0 means all datapoints).
-	* @param offset Starting point (in trainX, etc.) from where data is loaded.
-	*/
-	template<typename dataType, typename labelType>
-	void LoadImageData(arma::Mat<dataType>& X,
-					   arma::Mat<labelType>& y,
-					   bool train,
-					   size_t limit = 0,
-					   size_t offset = 0);
-	
+  /**
+  * Loads the name of validation files from the tiny imagenet dataset.
+  *
+  * @param folderPath Path of the training images.
+  */
+  void LoadValData(const std::string folderPath);
+  
+  /**
+  * Loads images into armadillo matrix.
+  *
+  * @param X Matrix where images are loaded.
+  * @param y Labels associated with the images
+  * @param train Boolean variable to identify the dataset type (i.e train/val).
+  * @param limit Number of datapoints to be stored (0 means all datapoints).
+  * @param offset Starting point (in trainX, etc.) from where data is loaded.
+  */
+  template<typename dataType, typename labelType>
+  void LoadImageData(arma::Mat<dataType>& X,
+             arma::Mat<labelType>& y,
+             bool train,
+             size_t limit = 0,
+             size_t offset = 0);
+  
 };
 
 #include "dataloader_impl.hpp"
