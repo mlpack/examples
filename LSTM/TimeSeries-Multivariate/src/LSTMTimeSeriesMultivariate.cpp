11b2f6cf0041e23266fe9ff20ee313cebb207193
Zoltan Somogyi <zsomogyi.be@gmail.com>
2019-12-31 10:30:55 +0100

    Update code

--- LSTM/TimeSeries-Multivariate/src/LSTMTimeSeriesMultivariate.cpp
+++ LSTM/TimeSeries-Multivariate/src/LSTMTimeSeriesMultivariate.cpp
@@ -14,7 +14,8 @@
  */
 
 /*
-NOTE: the data need to be sorted by date in ascending order! The RNN learns from oldest to newest!
+NOTE: the data need to be sorted by date in ascending order! The RNN learns from 
+oldest to newest!
 
 date  close  volume  open  high  low
 27-06-16  668.26  2632011  671  672.3  663.284
@@ -61,18 +62,19 @@ double MSE(arma::cube& pred, arma::cube& Y)
 }
 
 /*
- * The time series data for training the model contains the Closing stock price, the Volume of stocks traded,
- * Opening stock price, Highest stock price and Lowest stock price for 'rho' days in the past. 
- * The two target variables (multivariate) we want to predict are the Highest stock price and Lowest stock price
- * (high, low) for the next day! 
+ * The time series data for training the model contains the Closing stock price,
+ * the Volume of stocks traded, Opening stock price, Highest stock price and 
+ * Lowest stock price for 'rho' days in the past. The two target variables 
+ * (multivariate) we want to predict are the Highest stock price and Lowest 
+ * stock price (high, low) for the next day! 
  *
- * NOTE: Please note that we do not use the last input data point in the training because there is no target 
- *       (next day (high, low)) for that point!
+ * NOTE: Please note that we do not use the last input data point in the 
+ * training because there is no target (next day (high, low)) for that point!
  */
 template<typename InputDataType = arma::mat,
   typename DataType = arma::cube,
   typename LabelType = arma::cube>
-  void CreateTimeSeriesData(InputDataType dataset, DataType& X, LabelType& y, size_t rho)
+void CreateTimeSeriesData(InputDataType dataset, DataType& X, LabelType& y, size_t rho)
 {
   for (size_t i = 0; i < dataset.n_cols - rho; i++)
   {
@@ -82,77 +84,71 @@ template<typename InputDataType = arma::mat,
 }
 
 /*
- * This function saves the input data for prediction and the prediction results in CSV format. 
- * The prediction results are the (high, low) for the next day and comming from the last slice 
- * from the prediction.
+ * This function saves the input data for prediction and the prediction results 
+ * in CSV format. The prediction results are the (high, low) for the next day 
+ * and comming from the last slice of the prediction. The last 2 columns are 
+ * the predictions, the preceding columns are the data used to generate those 
+ * predictions.
  */
-void saveP(const std::string filename, const arma::cube& predictions, data::MinMaxScaler& scale,
+void saveAndResults(const std::string filename, const arma::cube& predictions, data::MinMaxScaler& scale,
   const arma::cube& testX)
 {
-  std::ofstream out(filename);
-  //we need to denormalize (inverse transform) the input data because it was normalized before training
-  mat tempX = testX.slice(testX.n_slices - 1);
-  scale.InverseTransform(tempX, tempX);
+  mat flatDataAndPreds = testX.slice(testX.n_slices - 1);
+  scale.InverseTransform(flatDataAndPreds, flatDataAndPreds);
 
-  for (size_t c = 0; c < tempX.n_cols - 1; ++c)
-  {
-    //the result we are looking for is in the last slice!
-    size_t i = predictions.n_slices - 1;
-    {
-      mat temp = predictions.slice(i);
+  //The prediction results are the (high, low) for the next day and comming from 
+  //the last slice from the prediction.
+  mat temp = predictions.slice(predictions.n_slices - 1);
 
-      // We need to denormalize the predictions also!
-      // NOTE: We add 3 extra rows here in order to recreate the input data structure used to 
-      // transform the data. This is needed in order to be able to use the right scaling 
-      // parameters for the specific column (stock high, low).
-      temp.insert_rows(0, 3, 0);
-      scale.InverseTransform(temp, temp);
-      
-      for (size_t r = 0; r < tempX.n_rows; ++r)
-      {
-        if (r > 0) out << ",";
-        out << tempX.at(r, c);
-      }
+  //NOTE: We add 3 extra rows here in order to recreate the input data 
+  //structure used to transform the data. This is needed in order to be able 
+  //to use the right scaling parameters for the specific column stock 
+  //high, low).
+  temp.insert_rows(0, 3, 0);
+  scale.InverseTransform(temp, temp);
 
-      //let us put one empty column between the input for the prediction and the prediction results
-      out << ",,";
-      for (size_t r = 3; r < temp.n_rows; ++r)
-      {
-        if (r > 3) out << ",";
-        out << temp.at(r, c);
-      }
+  //we add the prediction as the last two columns (stock high, low)
+  flatDataAndPreds.insert_rows(flatDataAndPreds.n_rows, temp.rows(temp.n_rows - 2, temp.n_rows - 1));
 
-      //Show the actual result on the screen also.
-      //NOTE: Please note that we do not have the last data point in the input for the prediction because 
-      //    we did not use it for the training, therefore the prediction result will be for the day before! 
-      //    In your own application you may of course load any dataset for prediction!
-      if (c == tempX.n_cols - 2)
-      {
-        cout << std::endl << "The predicted Google stock (high, low) for the last day is the following:" << std::endl;
-        for (size_t r = 3; r < temp.n_rows; ++r)
-        {
-          if (r > 3) cout << ",";
-          cout << temp.at(r, c);
-        }
-        cout << std::endl;
-      }
-    }
-    out << std::endl;
-  }
+  //we need to remove the last column because it was not used for training (no 
+  //next day to predict)
+  flatDataAndPreds.shed_col(flatDataAndPreds.n_cols - 1);
 
-  out.close();
+  //Save the data to file. The last columns are the predictions; the preceding 
+  //columns are the data used to generate those predictions.
+  data::Save(filename, flatDataAndPreds);
+
+  //Print the output to screen.
+  //NOTE: we do not have the last data point in the input for the prediction 
+  //because we did not use it for the training, therefore the prediction result 
+  //will be for the day before! In your own application you may of course load 
+  //any dataset for prediction!
+  std::cout << "The predicted Google stock (high, low) for the last day is: " << std::endl;
+  std::cout << "  (" << flatDataAndPreds(flatDataAndPreds.n_rows - 2, flatDataAndPreds.n_cols - 1) << " ,";
+  std::cout << flatDataAndPreds(flatDataAndPreds.n_rows - 1, flatDataAndPreds.n_cols - 1) << ")" << std::endl;
 }
 
 int main()
 {
-  //TODO: change this to your base data path!
-  const string basedatapath = "D:/sample-ml-app";
+  //Change the names of these files as necessary. They should be correct 
+  //already, if your program's working directory contains the data and/or model.
+  const string dataFile = "D:/_WORK5/mlpackn/sample-ml-app/sample-ml-app/data/Google2016-2019.csv";
+  // example: const string dataFile = "C:/mlpack-model-app/Google2016-2019.csv";
+  // example: const string dataFile = "/home/user/mlpack-model-app/Google2016-2019.csv";
+  const string modelFile = "D:/_WORK5/mlpackn/sample-ml-app/sample-ml-app/saved_models/lstm_multi.bin";
+  // example: const string modelFile = "C:/mlpack-model-app/lstm_multi.bin";
+  // example: const string modelFile = "/home/user/mlpack-model-app/lstm_multi.bin";
+  const string predFile = "D:/_WORK5/mlpackn/sample-ml-app/sample-ml-app/saved_models/lstm_multi_predictions.csv";
 
-  //TODO: if true the model will be trained; if false the saved model will be read and used for prediction
-  //NOTE: training the model may take a long time, therefore once it is trained you can set this to false
-  //    and use the model for prediction.
-  //NOTE: there is no error checking in this example to see if the trained model exists!
+  //If true the model will be trained; if false the saved model will be
+  //read and used for prediction
+  //NOTE: training the model may take a long time, therefore once it is 
+  //trained you can set this to false and use the model for prediction.
+  //NOTE: there is no error checking in this example to see if the trained
+  //model exists!
   const bool bTrain = true;
+  //you can load and further train a model
+  const bool bLoadAndTrain = false;
 
   // Testing data is taken from the dataset in this ratio.
   const double RATIO = 0.1;
@@ -167,7 +163,9 @@ int main()
   const double STEP_SIZE = 5e-5;
 
   //number of cells in the LSTM (hidden layers in standard terms)
-  //NOTE: you may play with this variable in order to further optimize the model
+  //NOTE: you may play with this variable in order to further optimize the model.
+  //(as more cells are added, accuracy is likely to go up, but training time may
+  //take longer)
   const int H1 = 25;
 
   // Number of data points in each iteration of SGD.
@@ -183,12 +181,12 @@ int main()
 
   // In Armadillo rows represent features, columns represent data points.
   cout << "Reading data ..." << endl;
-  data::Load(basedatapath + "/data/Google2016-2019.csv", dataset, true);
+  data::Load(dataFile, dataset, true);
 
-  // The CSV file has a header, so it is necessary to remove it. In Armadillo's representation 
-  // it is the first column.
-  // The first column in the CSV is the date which is not required, therefore removing it 
-  // also (first row in in arma::mat).
+  //The CSV file has a header, so it is necessary to remove it. In Armadillo's 
+  //representation it is the first column.
+  //The first column in the CSV is the date which is not required, therefore 
+  //removing it also (first row in in arma::mat).
   dataset = dataset.submat(1, 1, dataset.n_rows - 1, dataset.n_cols - 1);
 
   // Scale all data into the range (0, 1) for increased numerical stability.
@@ -199,8 +197,9 @@ int main()
   //we have 5 input data columns and 2 output columns (target)
   size_t inputSize = 5, outputSize = 2;
 
-  //We need to represent the input data for RNN in arma::cube (3D matrix)! The 3rd dimension is the 
-  //rho number of past data records the RNN uses for learning.
+  //We need to represent the input data for RNN in arma::cube (3D matrix)! The 
+  //3rd dimension is the rho number of past data records the RNN uses for 
+  //learning.
   arma::cube X, y;
   X.set_size(inputSize, dataset.n_cols - rho + 1, rho);
   y.set_size(outputSize, dataset.n_cols - rho + 1, rho);
@@ -217,10 +216,16 @@ int main()
   testY = y.subcube(span(), span(trainingSize, X.n_cols - 1), span());
 
   //only train the model if required  
-  if (bTrain) {
+  if (bTrain || bLoadAndTrain) {
     // RNN regression model.
     RNN<MeanSquaredError<>, HeInitialization> model(rho);
 
+  if (bLoadAndTrain) {
+    //the model will be trained further
+    std::cout << "Loading and further training model..." << std::endl;
+    data::Load(modelFile, "LSTMMulti", model);
+  }
+  else {
     //Model building.
     model.Add<IdentityLayer<> >();
     model.Add<LSTM<> >(inputSize, H1, maxRho);
@@ -232,6 +237,7 @@ int main()
     model.Add<LSTM<> >(H1, H1, maxRho);
     model.Add<LeakyReLU<> >();
     model.Add<Linear<> >(H1, outputSize);
+  }
 
     // Setting parameters Stochastic Gradient Descent (SGD) optimizer.
     SGD<AdamUpdate> optimizer(
@@ -266,19 +272,20 @@ int main()
 
     cout << "Finished training." << endl;
     cout << "Saving Model" << endl;
-    data::Save(basedatapath + "/saved_models/LSTMMulti.bin", "LSTMMulti", model);
-    std::cout << "Model saved in " << basedatapath << "/saved_models/LSTMMulti.bin" << std::endl;
+    data::Save(modelFile, "LSTMMulti", model);
+    std::cout << "Model saved in " << modelFile << std::endl;
   }
 
-  //NOTE: the below is added in order to show how in a real application the model would be saved, loaded and
-  //      then used for prediction.
-  //    Please note that we do not have the last data point in testX because we did not use it for the training,
-  //    therefore the prediction result will be for the day before! In your own application you may of course 
-  //    load any dataset.
-  // Load RNN model and use it for prediction
+  //NOTE: the below is added in order to show how in a real application the 
+  //model would be saved, loaded and then used for prediction. Please note that 
+  //we do not have the last data point in testX because we did not use it for 
+  //the training, therefore the prediction result will be for the day before!
+  //In your own application you may of course load any dataset.
+
+  //Load RNN model and use it for prediction
   RNN<MeanSquaredError<>, HeInitialization> modelP(rho);
   std::cout << "Loading model ..." << std::endl;
-  data::Load(basedatapath + "/saved_models/LSTMMulti.bin", "LSTMMulti", modelP);
+  data::Load(modelFile, "LSTMMulti", modelP);
   arma::cube predOutP;
   // Getting predictions on test data points.
   modelP.Predict(testX, predOutP);
@@ -286,8 +293,8 @@ int main()
   double testMSEP = MSE(predOutP, testY);
   cout << "Mean Squared Error on Prediction data points:= " << testMSEP << endl;
 
-  //save the output predictions
-  saveP(basedatapath + "/saved_models/LSTMMulti_predictions.csv", predOutP, scale, testX);
+  //save the output predictions and show the results
+  saveAndResults(predFile, predOutP, scale, testX);
 
   cout << "Ready!" << std::endl;
   getchar();
