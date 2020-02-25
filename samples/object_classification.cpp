f186053695531db583649054a89d4e7329f52326
kartikdutt18 <kartikdutt@live.in>
2020-02-25 19:45:57 +0530

    Include alexnet as template

--- samples/object_classification.cpp
+++ samples/object_classification.cpp
@@ -1,4 +1,10 @@
-#include<iostream>
+#include <ensmallen.hpp>
+#include <mlpack/core.hpp>
+#include <mlpack/core/data/split_data.hpp>
+#include <mlpack/methods/ann/layer/layer.hpp>
+#include <mlpack/methods/ann/init_rules/glorot_init.hpp>
+#include <mlpack/methods/ann/ffn.hpp>
+#include <models/alexnet.hpp>
 
 int main()
 {
