b34955ad6ca22546ce1a533cb49583192731804d
Omar Shrit <shrit@lri.fr>
2020-03-25 16:55:49 +0100

    Remove non used accuracy function.
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- mnist_batch_norm/kaggle_utils.hpp
+++ mnist_batch_norm/kaggle_utils.hpp
@@ -31,32 +31,12 @@ arma::Row<size_t> getLabels(const arma::mat& predOut)
   // in j-th column plus 1 (since column's elements are numbered from 0).
   for (size_t j = 0; j < predOut.n_cols; ++j)
   {
-    pred(j) = arma::as_scalar(arma::find(
-        arma::max(predOut.col(j)) == predOut.col(j), 1)) + 1;
+    pred(j) = arma::as_scalar(
+                  arma::find(arma::max(predOut.col(j)) == predOut.col(j), 1))
+              + 1;
   }
 
   return pred;
-}
-
-/**
- * Returns the accuracy (percentage of correct answers).
- * @param predLabels predicted labels of data points.
- * @param realY real labels (they are double because we usually read them from
- * CSV file that contain many other double values).
- * @return percentage of correct answers.
- */
-double accuracy(arma::Row<size_t> predLabels, const arma::mat& realY)
-{
-  // Calculating how many predicted classes are coincide with real labels.
-  size_t success = 0;
-  for (size_t j = 0; j < realY.n_cols; j++) {
-    if (predLabels(j) == std::round(realY(j))) {
-      ++success;
-    }
-  }
-
-  // Calculating percentage of correctly classified data points.
-  return (double)success / (double)realY.n_cols * 100.0;
 }
 
 /**
@@ -68,23 +48,24 @@ double accuracy(arma::Row<size_t> predLabels, const arma::mat& realY)
  * are expected to start from 1. At the same time classes of data points in
  * the file are going to start from 0 (as Kaggle usually expects)
  */
-void save(const std::string filename, std::string header,
-  const arma::Row<size_t>& predLabels)
+void save(const std::string filename,
+          std::string header,
+          const arma::Row<size_t>& predLabels)
 {
-	std::ofstream out(filename);
-	out << header << std::endl;
-	for (size_t j = 0; j < predLabels.n_cols; ++j)
-	{
-	  // j + 1 because Kaggle indexes start from 1
-	  // pred - 1 because 1st class is 0, 2nd class is 1 and etc.
-		out << j + 1 << "," << std::round(predLabels(j)) - 1;
+  std::ofstream out(filename);
+  out << header << std::endl;
+  for (size_t j = 0; j < predLabels.n_cols; ++j)
+  {
+    // j + 1 because Kaggle indexes start from 1
+    // pred - 1 because 1st class is 0, 2nd class is 1 and etc.
+    out << j + 1 << "," << std::round(predLabels(j)) - 1;
     // to avoid an empty line in the end of the file
-		if (j < predLabels.n_cols - 1)
-		{
-		  out << std::endl;
-		}
-	}
-	out.close();
+    if (j < predLabels.n_cols - 1)
+    {
+      out << std::endl;
+    }
+  }
+  out.close();
 }
 
 #endif
