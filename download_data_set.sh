2f9e6aad557e5df14501d9d0b781eee1e3d9e434
Omar Shrit <shrit@lri.fr>
2020-04-12 19:49:25 +0200

    Adding electricity consumption and stock exchange
    Only link is missing,
    
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- download_data_set.sh
+++ download_data_set.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "This script will download the entire dataset for all the available examples"
 echo "Create a dataset directory"
 mkdir -p data
@@ -21,6 +23,16 @@ echo "Convert mnist images into csv format"
 ../tools/convert_mnist_2_csv.py t10k-images-idx3-ubyte.gz
 ../tools/convert_mnist_2_csv.py t10k-labels-idx1-ubyte.gz
 
-# Where can we find other dataset online?
+echo "Download the electricty consumption example datasets"
+curl https:// --output electricty_consumption_dataset.tar.gz
+echo "Extracting"
+tar -xzf electricty_consumption_dataset.tar.gz 
+echo "Download the stock exchange example datasets"
+curl https:// --output stock_exchange.tar.gz
+echo "Extracting"
+tar -xzf stock_exchange.tar.gz
 
+echo "Cleaning"
+rm *.gz
 
+popd
