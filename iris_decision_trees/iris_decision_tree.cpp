ccd50c42240cebcdf6aebc909916deb510a5bee8
Param-29 <param.mirani17@vit.edu>
2020-04-25 10:30:01 +0530

    Comments added.

--- iris_decision_trees/iris_decision_tree.cpp
+++ iris_decision_trees/iris_decision_tree.cpp
@@ -10,15 +10,22 @@ using namespace mlpack::tree;
 
 int main(){
 
+  // Dataset is randomly split into validation
+  // and training parts in the following ratio.
   double RATIO = 0.1;
-
+	
+	// Labeled dataset that contains data for training is loaded from CSV file,
+  // rows represent features, columns represent data points.
   arma::mat dataset;
   data::Load("../data/iris_data.csv", dataset, true);
 
-  arma::mat train, valid;
+  // The dataset is divided into 2 datasets, i.e train and valid
+	arma::mat train, valid;
   data::Split(dataset, train, valid, RATIO);
 
-  arma::mat trainX = train.submat(1, 0, train.n_rows - 1,
+  // trainX and validX contain indepedent variables (i.e features) 
+	// on which model will be trained 
+	arma::mat trainX = train.submat(1, 0, train.n_rows - 1,
       train.n_cols - 1);
   arma::mat validX = valid.submat(1, 0, valid.n_rows - 1,
       valid.n_cols - 1);
@@ -26,29 +33,40 @@ int main(){
 
   arma::Row<size_t> trainY , validY;
 
+	// trainY and validY contain dependent features.
   trainY = arma::conv_to<arma::Row<size_t>>::from(
           train.row(train.n_rows-1));
-
   validY = arma::conv_to<arma::Row<size_t>>::from(
           valid.row(train.n_rows-1));
+
+		// Number of classes in the dataset.			
     const size_t numClasses = arma::max(arma::max(trainY)) + 1;
+		// Minimum leaf size for decision trees.
     const size_t minLeafSize = 3;
+		// Maximum depth of decision tree.
     const size_t maxDepth = 15;
-    const double minimumGainSplit =-1;
 
-    DecisionTree<> model = DecisionTree<>(  trainX, 
-                                            trainY, 
-                                            numClasses, 
-                                            minLeafSize, 
-                                            minimumGainSplit, 
-                                            maxDepth); 
+		// Minimum gain required for split
+    const double minGainSplit =-1;
 
-  arma::Row<size_t> predictions;
+		// Building the model.
+    DecisionTree<> model = DecisionTree<>(  trainX,				// Independent variables
+                                            trainY,				// Dependent variables
+                                            numClasses, 	// number of classes
+                                            minLeafSize,	// minimum leaf size
+                                            minGainSplit,	// minimum gain split 
+                                            maxDepth); 		// maximum depth
+
+  // Predictions made by model.
+	arma::Row<size_t> predictions;
+	// probabilites for each data-point for each class.
   arma::mat probabilities;
 
-  model.Classify(validX, predictions, probabilities);
+  // classify function to predict on un-seen data.
+	model.Classify(validX, predictions, probabilities);
 
-  size_t correct = 0;
+  // calculating number of correct points.
+	size_t correct = 0;
       for (size_t i = 0; i < validY.n_cols; ++i)
         if (predictions[i] == validY[i])
           ++correct;
