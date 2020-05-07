752f08f4041bb3fb789a6e9d49e171d2d4bfbadc
Omar Shrit <shrit@lri.fr>
2020-05-08 01:39:24 +0200

    Small sketch of python script instead of shell, not finished yet
    Still need to convert the ubyte into csv for the mnist dataset,
    Other dataset should work fine
    
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- download_data_set.py
+++ download_data_set.py
@@ -1,64 +1,98 @@
-#!/bin/bash
+#!/usr/bin/python
 # Download required dataset for each example.
+# Author: Omar Shrit
 
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]
-then
-  echo "Usage: $0 [-n <dataset_name>]"
-  echo "Available options:"
-  echo "mnist : will download mnist dataset"
-  echo "electricty : will download electricty_consumption_dataset"
-  echo "stock : will download stock_exchange dataset"
-  echo "all : will download all datasets for all examples"
-  exit 1
-fi
+import argparse
+import gzip
+import sys
+import textwrap
+import requests
+import shutil
 
-echo "Create a dataset directory"
-mkdir -p data
-pushd data &>/dev/null
+def ungzip(gzip_file, outputfile):
+  with open(outputfile, 'wb') as f_in, gzip.open(gzip_file, 'rb') as f_out:
+    shutil.copyfileobj(f_out, f_in)  
 
-function mnist_dataset() {
+def mnist_dataset():
+  print("Start downloading the mnist dataset")
 
-  echo "Start downloading the mnist dataset"
-
-  curl http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz --output train-images-idx3-ubyte.gz && gunzip train-images-idx3-ubyte.gz
-
-  curl http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz --output train-labels-idx1-ubyte.gz && gunzip train-labels-idx1-ubyte.gz
-
-  curl http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz  --output t10k-images-idx3-ubyte.gz && gunzip t10k-images-idx3-ubyte.gz
-
-  curl http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz  --output t10k-labels-idx1-ubyte.gz && gunzip t10k-labels-idx1-ubyte.gz
-
-  echo "Convert mnist images into csv format"
-  ../tools/convert_mnist_2_csv.py train-images-idx3-ubyte.gz
-  ../tools/convert_mnist_2_csv.py train-labels-idx1-ubyte.gz
-  ../tools/convert_mnist_2_csv.py t10k-images-idx3-ubyte.gz
-  ../tools/convert_mnist_2_csv.py t10k-labels-idx1-ubyte.gz
-}
-
-function electricty_consumption_dataset() {
-
-  echo "Download the electricty consumption example datasets"
-  curl https:// --output electricty_consumption_dataset.tar.gz
+  train_features = requests.get(
+  "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz")
+  open("train_features.gz", 'wb').write(train_features.content)
+  ungzip("train_features.gz", "train_features")
   
-  tar -xzf electricty_consumption_dataset.tar.gz
-}
+  train_labels = requests.get(
+  "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz")
+  open("train_labels.gz", 'wb').write(train_labels.content)
+  ungzip("train_labels.gz", "train_labels")
 
-function stock_exchange_dataset() {
-  echo "Download the stock exchange example datasets"
-  curl https:// --output stock_exchange.tar.gz
+  test_features = requests.get(
+  "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz")
+  open("test_features.gz", 'wb').write(test_features.content)
+  ungzip("test_features.gz", "test_features")
+  
+  test_labels = requests.get(
+  "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz")
+  open("test_labels.gz", 'wb').write(test_labels.content)
+  ungzip("test_labels.gz", "test_labels")
 
-  tar -xzf stock_exchange.tar.gz
-}
+  # Still need to Convert mnist images into csv format, not finished yet
+ 
 
-function all_dataset() {
-  mnist
-  electricty_consumption_dataset
-  stock_exchange_dataset
-}
+def electricty_consumption_dataset():
+  print("Download the electricty consumption example datasets")
+  electricty = request.get("https://www.mlpack.org/datasets/examples/electricity-usage.csv")
+  open("data/electricity-usage.csv", 'wb').write(stock.content)
 
-function cleanup() {
-  rm -rf data
-}
+def stock_exchange_dataset():
+  print("Download the stock exchange example datasets")
+  stock = requests.get("https://www.mlpack.org/datasets/examples/Google2016-2019.csv")
+  open("data/stock.csv", 'wb').write(stock.content)
 
-popd &>/dev/null
+def all_datasets():
+  mnist()
+  electricty_consumption_dataset()
+  stock_exchange_dataset()
 
+# function cleanup() {
+#   rm -rf data
+# }
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        prog='Download dataset script',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+        Dataset script!
+        --------------------------------
+        This script is used to download the dataset required to run
+        mlpack examples.
+        It can be used to download one dataset at the time,
+        or all of them at the same time.
+
+        Usage: --dataset_name dataset_name
+        Available options:
+        mnist : will download mnist dataset
+        electricty : will download electricty_consumption_dataset
+        stock : will download stock_exchange dataset
+        all : will download all datasets for all examples
+        '''))
+ 
+    parser.add_argument('--dataset_name', metavar="dataset name", type=str, help="Enter dataset name to download")
+    args = parser.parse_args()
+
+    if len(sys.argv)==1:
+      parser.print_help(sys.stderr)
+      sys.exit(1)
+
+    if args.dataset_name:
+      if args.dataset_name == "mnist":
+        mnist_dataset()
+      elif args.dataset_name == "electricty":
+        electricty_consumption_dataset()
+      elif args.dataset_name == "stock":
+        stock_exchange_dataset()
+      elif args.dataset_name == "all":
+        all_datasets()
+ 
