c2104420d14263edf6bc95c13b0dd7cf3ec01eae
Omar Shrit <shrit@lri.fr>
2020-05-08 13:42:07 +0200

    A woking version:
    * Added convert ubyte to csv borrowed from Jopseph Redmon webpage
    * Added iris dataset
    * Clean all compressed files
    * Clean all ubytes files
    * Create dataset directory only once
    
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- download_data_set.py
+++ download_data_set.py
@@ -4,59 +4,112 @@
 
 import argparse
 import gzip
+import os
 import sys
+import tarfile
 import textwrap
 import requests
 import shutil
 
+''' This function is originally written by Joseph Redmon (pjreddie).
+    The original source code can be found here:
+      https://pjreddie.com/projects/mnist-in-csv/
+'''
+def convert(imgf, labelf, outf, n):
+    f = open(imgf, "rb")
+    o = open(outf, "w")
+    l = open(labelf, "rb")
+
+    f.read(16)
+    l.read(8)
+    images = []
+
+    for i in range(n):
+        image = [ord(l.read(1))]
+        for j in range(28*28):
+            image.append(ord(f.read(1)))
+        images.append(image)
+
+    for image in images:
+        o.write(",".join(str(pix) for pix in image)+"\n")
+    f.close()
+    o.close()
+    l.close()
+
+def create_dataset_dir():
+  if os.path.exists("data"):
+    os.chdir("data")
+  else:
+    os.mkdir("data")
+    os.chdir("data")
+
+def clean():
+  dataset = "."
+  files = os.listdir(dataset)
+  for f in files:
+    if f.endswith(".gz"):
+      os.remove(os.path.join(dataset, f))
+    elif f.endswith(".tar.gz"):
+      os.remove(os.path.join(dataset, f))
+    elif f.endswith(".ubytes"):
+      os.remove(os.path.join(dataset, f))      
+
 def ungzip(gzip_file, outputfile):
   with open(outputfile, 'wb') as f_in, gzip.open(gzip_file, 'rb') as f_out:
-    shutil.copyfileobj(f_out, f_in)  
+    shutil.copyfileobj(f_out, f_in) 
 
 def mnist_dataset():
   print("Start downloading the mnist dataset")
-
   train_features = requests.get(
   "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz")
   open("train_features.gz", 'wb').write(train_features.content)
-  ungzip("train_features.gz", "train_features")
+  ungzip("train_features.gz", "train_features.ubytes")
   
   train_labels = requests.get(
   "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz")
   open("train_labels.gz", 'wb').write(train_labels.content)
-  ungzip("train_labels.gz", "train_labels")
+  ungzip("train_labels.gz", "train_labels.ubytes")
 
   test_features = requests.get(
   "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz")
   open("test_features.gz", 'wb').write(test_features.content)
-  ungzip("test_features.gz", "test_features")
+  ungzip("test_features.gz", "test_features.ubytes")
   
   test_labels = requests.get(
   "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz")
   open("test_labels.gz", 'wb').write(test_labels.content)
-  ungzip("test_labels.gz", "test_labels")
+  ungzip("test_labels.gz", "test_labels.ubytes")
 
-  # Still need to Convert mnist images into csv format, not finished yet
- 
+  convert("train_features.ubytes", "train_labels.ubytes",
+  "mnist_train.csv", 60000)
+  convert("test_features.ubytes", "test_labels.ubytes",
+  "mnist_test.csv", 10000)
+  clean()
 
-def electricty_consumption_dataset():
+def electricity_consumption_dataset():
   print("Download the electricty consumption example datasets")
-  electricty = request.get("https://www.mlpack.org/datasets/examples/electricity-usage.csv")
-  open("data/electricity-usage.csv", 'wb').write(stock.content)
+  electricity = requests.get("https://www.mlpack.org/datasets/examples/electricity-usage.csv")
+  open("electricity-usage.csv", 'wb').write(electricity.content)
 
 def stock_exchange_dataset():
   print("Download the stock exchange example datasets")
   stock = requests.get("https://www.mlpack.org/datasets/examples/Google2016-2019.csv")
-  open("data/stock.csv", 'wb').write(stock.content)
+  open("stock.csv", 'wb').write(stock.content)
 
+def iris_dataset():
+  print("Downloading iris datasets...")
+  iris = requests.get("https://www.mlpack.org/datasets/iris.tar.gz")
+  open("iris.tar.gz", 'wb').write(iris.content)
+  tar = tarfile.open("iris.tar.gz", "r:gz")
+  tar.extractall()
+  tar.close()
+  clean()
+  
 def all_datasets():
-  mnist()
-  electricty_consumption_dataset()
+  mnist_dataset()
+  electricity_consumption_dataset()
   stock_exchange_dataset()
-
-# function cleanup() {
-#   rm -rf data
-# }
+  iris_dataset()
 
 if __name__ == '__main__':
 
@@ -74,7 +127,7 @@ if __name__ == '__main__':
         Usage: --dataset_name dataset_name
         Available options:
         mnist : will download mnist dataset
-        electricty : will download electricty_consumption_dataset
+        electricity : will download electricty_consumption_dataset
         stock : will download stock_exchange dataset
         all : will download all datasets for all examples
         '''))
@@ -88,11 +141,18 @@ if __name__ == '__main__':
 
     if args.dataset_name:
       if args.dataset_name == "mnist":
+        create_dataset_dir()
         mnist_dataset()
-      elif args.dataset_name == "electricty":
-        electricty_consumption_dataset()
+      elif args.dataset_name == "electricity":
+        create_dataset_dir()
+        electricity_consumption_dataset()
       elif args.dataset_name == "stock":
+        create_dataset_dir()
         stock_exchange_dataset()
+      elif args.dataset_name == "iris":
+        create_dataset_dir()
+        iris_dataset()
       elif args.dataset_name == "all":
+        create_dataset_dir()
         all_datasets()
  
