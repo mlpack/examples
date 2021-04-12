4b8c7fcc9e01de1c9163ea2b4db4154f4bf7f71d
davidportlouis <dportlouis@gmail.com>
2021-04-12 14:50:45 +0530

    added inline comments for explanation

--- salary_prediction_with_linear_regression/salary_prediction_linear_regression.cc
+++ salary_prediction_with_linear_regression/salary_prediction_linear_regression.cc
@@ -19,13 +19,16 @@ namespace plt = matplotlibcpp;
 int main() {
 
   // Loading data from csv into matrix
+
   arma::mat inputs;
   data::Load("Salary.csv", inputs);
 
   // Dropping first row as they represent headers
+
   inputs.shed_col(0);
 
   // Print the first 5 rows of the input data
+
   std::cout<<std::setw(18)<<"Years Of Experience"<<std::setw(10)<<"Salary"<<std::endl;
   std::cout<<inputs.submat(0, 0, inputs.n_rows-1, 5).t()<<std::endl;
 
@@ -34,38 +37,59 @@ int main() {
   std::vector<double> x = arma::conv_to<std::vector<double>>::from(inputs.row(0));
   std::vector<double> y = arma::conv_to<std::vector<double>>::from(inputs.row(1));
 
-  plt::scatter(x, y, 5);
+  plt::scatter(x, y, 12, {{"color", "coral"}});
+  plt::xlabel("Years of Experience");
+  plt::ylabel("Salary in $");
+  plt::title("Experience vs. Salary");
   plt::show();
 
   // Split the data into features (X) and target (y) variables
-  // Labels are the last row
+  // targets are the last row
+
   arma::Row<size_t> targets = arma::conv_to<arma::Row<size_t>>::from(inputs.row(inputs.n_rows - 1));
+
   // Labels are dropped from the originally loaded data to be used as features
+
   inputs.shed_row(inputs.n_rows - 1);
 
   // Split the dataset using mlpack
+
   arma::mat Xtrain;
   arma::mat Xtest;
   arma::Row<size_t> Ytrain;
   arma::Row<size_t> Ytest;
   data::Split(inputs, targets, Xtrain, Xtest, Ytrain, Ytest, 0.4);
 
+  // Convert armadillo Rows into rowvec. (Required by LinearRegression API in this format)
+
   arma::rowvec y_train = arma::conv_to<arma::rowvec>::from(Ytrain);
   arma::rowvec y_test = arma::conv_to<arma::rowvec>::from(Ytest);
 
   // Create and Train Linear Regression model
+
   LinearRegression lr(Xtrain, y_train, 0.5);
   
+  // Make predictions for test data points
+
   arma::rowvec y_preds;
   lr.Predict(Xtest, y_preds);
+
+  // convert armadillo vectors and matrices to vector for plotting
 
   std::vector<double> x_test = arma::conv_to<std::vector<double>>::from(Xtest);
   std::vector<double> y_t = arma::conv_to<std::vector<double>>::from(y_test);
   std::vector<double> y_p = arma::conv_to<std::vector<double>>::from(y_preds);
 
-  plt::scatter(x_test, y_t, 5);
+  // Visualizing Predicted datapoints
+
+  plt::scatter(x_test, y_t, 12, {{"color", "coral"}});
   plt::plot(x_test,y_p);
+  plt::xlabel("Years of Experience");
+  plt::ylabel("Salary in $");
+  plt::title("Predicted Experience vs. Salary");
   plt::show();
+
+  // Model evaluation metrics
 
   std::cout<<"Mean Absolute Error: "<<arma::mean(arma::abs(y_preds - y_test))<<std::endl;
   std::cout<<"Mean Squared Error: "<<arma::mean(arma::pow(y_preds - y_test,2))<<std::endl;
