56780f1cc1c2f29525c4812fe4305258411151e9
NanuSai <gnanasai.n18@iiits.in>
2021-05-14 07:31:55 +0530

    normalize

--- utils/portfolio.py
+++ utils/portfolio.py
@@ -7,5 +7,9 @@ def cportfolio(stocks, start, end, filename='portfolio.csv'):
   test = test['Adj Close']
 
   returns = np.log(test/test.shift(1))
+  returns.reset_index(inplace=True)
+  # Drop the first row since it contains NaN.
   returns = returns.iloc[1:]
+  # Normalize dates.
+  returns['Date'] = returns['Date'].apply(lambda x : x.strftime('%Y%m%d'))
   returns.to_csv(filename, header=False, index=False)
