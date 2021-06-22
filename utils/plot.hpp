17c8c9e9a32fc6a86a5e1a325993ce686d523d84
davidportlouis <dportlouis@gmail.com>
2021-06-22 08:45:06 +0530

    completed c++ notebook

--- utils/plot.hpp
+++ utils/plot.hpp
@@ -5,66 +5,38 @@
 #include <Python.h>
 #include <string>
 
-int scatter(const std::string fname, const std::string type, const int figWidth = 26, const int figHeight = 7) {
+int scatter(const std::string fname, const std::string type,const std::string figTitle, const int figWidth = 26, const int figHeight = 7) {
 
     PyObject *pName, *pModule, *pFunc, *pArgs, *pValue;
 
     Py_Initialize();
     PyRun_SimpleString("import sys");
     PyRun_SimpleString("sys.path.append(\"../utils/\")");
+    
     pName = PyUnicode_DecodeFSDefault("plot");
     pModule = PyImport_Import(pName);
-    //Py_DECREF(pName);
     
     pFunc = PyObject_GetAttrString(pModule, "cscatter");
-    pArgs = PyTuple_New(4);
+    
+    pArgs = PyTuple_New(5);
+    
     PyObject* pFname = PyString_FromString(fname.c_str());
     PyTuple_SetItem(pArgs, 0, pFname);
+    
     PyObject* pType = PyString_FromString(type.c_str());
     PyTuple_SetItem(pArgs, 1, pType);
+    
+    PyObject* pFigTitle = PyString_FromString(figTitle.c_str());
+    PyTuple_SetItem(pArgs, 2, pFigTitle);
+    
     PyObject* pFigWidth = PyLong_FromLong(figWidth);
-    PyTuple_SetItem(pArgs, 2, pFigWidth);
+    PyTuple_SetItem(pArgs, 3, pFigWidth);
+    
     PyObject* pFigHeight = PyLong_FromLong(figHeight);
-    PyTuple_SetItem(pArgs, 3, pFigHeight);
+    PyTuple_SetItem(pArgs, 4, pFigHeight);
+    
     pValue = PyObject_CallObject(pFunc, pArgs);
-
-    /*
-    if(pModule != NULL) {
-        
-        if(pFunc && PyCallable_Check(pFunc)) {
-            std::cout << "Callable Method Found :)" << std::endl;
-    */
-            
-            
-//             /*PyObject* pXdim = PyLong_FromLong(xdim);
-//             PyTuple_SetItem(pArgs, 1, pXdim);
-//             PyObject* pYdim = PyLong_FromLong(ydim);
-//             PyTuple_SetItem(pArgs, 2, pYdim);
-//             */
-            
-            
-//             Py_DECREF(pArgs);
-//             if(pValue != NULL) {
-//                 Py_DECREF(pValue);
-//             }else{
-//                 Py_DECREF(pFunc);
-//                 Py_DECREF(pModule);
-//                 PyErr_Print();
-//                 fprintf(stderr, "Call failed\n");
-//                 return 1;
-//             }
-    /*
-        }else{
-            if(PyErr_Occurred())
-                PyErr_Print();
-        }
-        Py_XDECREF(pFunc);
-        Py_DECREF(pModule);
-    }else{
-        PyErr_Print();
-        return 1;
-    }
-        */
+    
     return 0;
 }
 
@@ -75,63 +47,34 @@ int barplot(const std::string fname, const std::string x, const std::string y, c
     Py_Initialize();
     PyRun_SimpleString("import sys");
     PyRun_SimpleString("sys.path.append(\"./\")");
+    
     pName = PyUnicode_DecodeFSDefault("plot");
     pModule = PyImport_Import(pName);
-    //Py_DECREF(pName);
     
     pFunc = PyObject_GetAttrString(pModule, "cbarplot");
+    
     pArgs = PyTuple_New(6);
+    
     PyObject* pFname = PyString_FromString(fname.c_str());
     PyTuple_SetItem(pArgs, 0, pFname);
+    
     PyObject* pX = PyString_FromString(x.c_str());
     PyTuple_SetItem(pArgs, 1, pX);
+    
     PyObject* pY = PyString_FromString(y.c_str());
     PyTuple_SetItem(pArgs, 2, pY);
+    
     PyObject* pFigTitle = PyString_FromString(figTitle.c_str());
     PyTuple_SetItem(pArgs, 3, pFigTitle);
+    
     PyObject* pFigWidth = PyLong_FromLong(figWidth);
     PyTuple_SetItem(pArgs, 4, pFigWidth);
+    
     PyObject* pFigHeight = PyLong_FromLong(figHeight);
     PyTuple_SetItem(pArgs, 5, pFigHeight);
+    
     pValue = PyObject_CallObject(pFunc, pArgs);
 
-    /*
-    if(pModule != NULL) {
-        
-        if(pFunc && PyCallable_Check(pFunc)) {
-            std::cout << "Callable Method Found :)" << std::endl;
-    */
-            
-            
-//             /*PyObject* pXdim = PyLong_FromLong(xdim);
-//             PyTuple_SetItem(pArgs, 1, pXdim);
-//             PyObject* pYdim = PyLong_FromLong(ydim);
-//             PyTuple_SetItem(pArgs, 2, pYdim);
-//             */
-            
-            
-//             Py_DECREF(pArgs);
-//             if(pValue != NULL) {
-//                 Py_DECREF(pValue);
-//             }else{
-//                 Py_DECREF(pFunc);
-//                 Py_DECREF(pModule);
-//                 PyErr_Print();
-//                 fprintf(stderr, "Call failed\n");
-//                 return 1;
-//             }
-    /*
-        }else{
-            if(PyErr_Occurred())
-                PyErr_Print();
-        }
-        Py_XDECREF(pFunc);
-        Py_DECREF(pModule);
-    }else{
-        PyErr_Print();
-        return 1;
-    }
-        */
     return 0;
 }
 
@@ -142,124 +85,66 @@ int heatmap(const std::string fname, const std::string colorMap, const std::stri
     Py_Initialize();
     PyRun_SimpleString("import sys");
     PyRun_SimpleString("sys.path.append(\"./\")");
+    
     pName = PyUnicode_DecodeFSDefault("plot");
     pModule = PyImport_Import(pName);
-    //Py_DECREF(pName);
     
     pFunc = PyObject_GetAttrString(pModule, "cheatmap");
+    
     pArgs = PyTuple_New(6);
+    
     PyObject* pFname = PyString_FromString(fname.c_str());
     PyTuple_SetItem(pArgs, 0, pFname);
+    
     PyObject* pColorMap = PyString_FromString(colorMap.c_str());
     PyTuple_SetItem(pArgs, 1, pColorMap);
+    
     PyObject* pAnnotation = PyBool_FromLong(annotation);
     PyTuple_SetItem(pArgs, 2, pAnnotation);
+    
     PyObject* pFigTitle = PyString_FromString(figTitle.c_str());
     PyTuple_SetItem(pArgs, 3, pFigTitle);
+    
     PyObject* pFigWidth = PyLong_FromLong(figWidth);
     PyTuple_SetItem(pArgs, 4, pFigWidth);
+    
     PyObject* pFigHeight = PyLong_FromLong(figHeight);
     PyTuple_SetItem(pArgs, 5, pFigHeight);
+    
     pValue = PyObject_CallObject(pFunc, pArgs);
 
-    /*
-    if(pModule != NULL) {
-        
-        if(pFunc && PyCallable_Check(pFunc)) {
-            std::cout << "Callable Method Found :)" << std::endl;
-    */
-            
-            
-//             /*PyObject* pXdim = PyLong_FromLong(xdim);
-//             PyTuple_SetItem(pArgs, 1, pXdim);
-//             PyObject* pYdim = PyLong_FromLong(ydim);
-//             PyTuple_SetItem(pArgs, 2, pYdim);
-//             */
-            
-            
-//             Py_DECREF(pArgs);
-//             if(pValue != NULL) {
-//                 Py_DECREF(pValue);
-//             }else{
-//                 Py_DECREF(pFunc);
-//                 Py_DECREF(pModule);
-//                 PyErr_Print();
-//                 fprintf(stderr, "Call failed\n");
-//                 return 1;
-//             }
-    /*
-        }else{
-            if(PyErr_Occurred())
-                PyErr_Print();
-        }
-        Py_XDECREF(pFunc);
-        Py_DECREF(pModule);
-    }else{
-        PyErr_Print();
-        return 1;
-    }
-        */
     return 0;
 }
 
-int lmplot(const std::string fname, const int figWidth = 6, const int figHeight = 7) {
+int lmplot(const std::string fname, const std::string figTitle, const int figWidth = 6, const int figHeight = 7) {
 
     PyObject *pName, *pModule, *pFunc, *pArgs, *pValue;
 
     Py_Initialize();
     PyRun_SimpleString("import sys");
     PyRun_SimpleString("sys.path.append(\"./\")");
+    
     pName = PyUnicode_DecodeFSDefault("plot");
     pModule = PyImport_Import(pName);
-    //Py_DECREF(pName);
     
     pFunc = PyObject_GetAttrString(pModule, "clmplot");
-    pArgs = PyTuple_New(3);
+    
+    pArgs = PyTuple_New(4);
+    
     PyObject* pFname = PyString_FromString(fname.c_str());
     PyTuple_SetItem(pArgs, 0, pFname);
+    
+    PyObject* pFigTitle = PyString_FromString(figTitle.c_str());
+    PyTuple_SetItem(pArgs, 1, pFigTitle);
+    
     PyObject* pFigWidth = PyLong_FromLong(figWidth);
-    PyTuple_SetItem(pArgs, 1, pFigWidth);
+    PyTuple_SetItem(pArgs, 2, pFigWidth);
+    
     PyObject* pFigHeight = PyLong_FromLong(figHeight);
-    PyTuple_SetItem(pArgs, 2, pFigHeight);
+    PyTuple_SetItem(pArgs, 3, pFigHeight);
+    
     pValue = PyObject_CallObject(pFunc, pArgs);
 
-    /*
-    if(pModule != NULL) {
-        
-        if(pFunc && PyCallable_Check(pFunc)) {
-            std::cout << "Callable Method Found :)" << std::endl;
-    */
-            
-            
-//             /*PyObject* pXdim = PyLong_FromLong(xdim);
-//             PyTuple_SetItem(pArgs, 1, pXdim);
-//             PyObject* pYdim = PyLong_FromLong(ydim);
-//             PyTuple_SetItem(pArgs, 2, pYdim);
-//             */
-            
-            
-//             Py_DECREF(pArgs);
-//             if(pValue != NULL) {
-//                 Py_DECREF(pValue);
-//             }else{
-//                 Py_DECREF(pFunc);
-//                 Py_DECREF(pModule);
-//                 PyErr_Print();
-//                 fprintf(stderr, "Call failed\n");
-//                 return 1;
-//             }
-    /*
-        }else{
-            if(PyErr_Occurred())
-                PyErr_Print();
-        }
-        Py_XDECREF(pFunc);
-        Py_DECREF(pModule);
-    }else{
-        PyErr_Print();
-        return 1;
-    }
-        */
     return 0;
 }
 
@@ -270,59 +155,28 @@ int histplot(const std::string fname, const std::string figTitle, const int figW
     Py_Initialize();
     PyRun_SimpleString("import sys");
     PyRun_SimpleString("sys.path.append(\"./\")");
+    
     pName = PyUnicode_DecodeFSDefault("plot");
     pModule = PyImport_Import(pName);
-    //Py_DECREF(pName);
     
     pFunc = PyObject_GetAttrString(pModule, "chistplot");
+    
     pArgs = PyTuple_New(4);
+    
     PyObject* pFname = PyString_FromString(fname.c_str());
     PyTuple_SetItem(pArgs, 0, pFname);
+    
     PyObject* pFigTitle = PyString_FromString(figTitle.c_str());
     PyTuple_SetItem(pArgs, 1, pFigTitle);
+    
     PyObject* pFigWidth = PyLong_FromLong(figWidth);
     PyTuple_SetItem(pArgs, 2, pFigWidth);
+    
     PyObject* pFigHeight = PyLong_FromLong(figHeight);
     PyTuple_SetItem(pArgs, 3, pFigHeight);
+    
     pValue = PyObject_CallObject(pFunc, pArgs);
 
-    /*
-    if(pModule != NULL) {
-        
-        if(pFunc && PyCallable_Check(pFunc)) {
-            std::cout << "Callable Method Found :)" << std::endl;
-    */
-            
-            
-//             /*PyObject* pXdim = PyLong_FromLong(xdim);
-//             PyTuple_SetItem(pArgs, 1, pXdim);
-//             PyObject* pYdim = PyLong_FromLong(ydim);
-//             PyTuple_SetItem(pArgs, 2, pYdim);
-//             */
-            
-            
-//             Py_DECREF(pArgs);
-//             if(pValue != NULL) {
-//                 Py_DECREF(pValue);
-//             }else{
-//                 Py_DECREF(pFunc);
-//                 Py_DECREF(pModule);
-//                 PyErr_Print();
-//                 fprintf(stderr, "Call failed\n");
-//                 return 1;
-//             }
-    /*
-        }else{
-            if(PyErr_Occurred())
-                PyErr_Print();
-        }
-        Py_XDECREF(pFunc);
-        Py_DECREF(pModule);
-    }else{
-        PyErr_Print();
-        return 1;
-    }
-        */
     return 0;
 }
 
--- utils/plot.py
+++ utils/plot.py
@@ -2,9 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-
-def cscatter(filename: str, type_: str, figWidth: int, figHeight: int) -> None:
+def cscatter(filename: str, type_: str, figTitle: str, figWidth: int, figHeight: int) -> None:
+    """
+    creates a scatter plot of size figWidth & figHeight, named
+    figTitle and saves it.
+    """
     sns.set(color_codes=True)
     df = pd.read_csv(filename, parse_dates=["Date"])
     fig = plt.figure(figsize=(figWidth, figHeight))
@@ -13,44 +15,57 @@ def cscatter(filename: str, type_: str, figWidth: int, figHeight: int) -> None:
     plt.xlabel("Date")
     plt.ylabel("Average Price (USD)")
     plt.title(f"Average Price of {type_} Avocados Over Time")
-    plt.savefig(f"cscatter_{type_}.png")
+    plt.savefig(f"{figTitle}.png")
     plt.close()
 
 def cbarplot(filename: str, x: str, y: str, figTitle: str, figWidth: int, figHeight: int) -> None:
+    """
+    Creates a bar plot of size figWidth & figHeight, named
+    figTitle between x & y.
+    """
     sns.set(color_codes=True)
     df = pd.read_csv(filename, parse_dates=["Date"])
     fig = plt.figure(figsize=(figWidth, figHeight))
     ax = sns.barplot(x=x, y=y, data=df)
     plt.title(figTitle)
-    plt.savefig(f"cbarplot_{figTitle}.png")
+    plt.savefig(f"{figTitle}.png")
     plt.close()
     
 def cheatmap(filename: str, cmap: str, annotate: bool, figTitle: str, figWidth: int, figHeight: int) -> None:
+    """
+    Creates a heatmap (correlation map) of the dataset and saves it.
+    """
     sns.set(color_codes=True)
     df = pd.read_csv(filename, parse_dates=["Date"])
     df = df.drop("Unnamed: 0", axis=1)
     fig = plt.figure(figsize=(figWidth, figHeight))
     ax = sns.heatmap(df.corr(), cmap=cmap, annot=annotate)
     plt.title(figTitle)
-    plt.savefig(f"cheatmap_{figTitle}.png")
+    plt.savefig(f"{figTitle}.png")
     plt.close()
     
-def clmplot(filename: str, figWidth: int, figHeight: int) -> None:
+def clmplot(filename: str, figTitle: str, figWidth: int, figHeight: int) -> None:
+    """
+    Generates a regression plot on the given dataset and saves it.
+    """
     sns.set(color_codes=True)
     df = pd.read_csv(filename)
     df.columns = ["Y_Test", "Y_Preds"]
     fig = plt.figure(figsize=(figWidth, figHeight))
     ax = sns.lmplot(x="Y_Test", y="Y_Preds", data=df)
-    plt.savefig(f"clmplot_{filename}.png")
+    plt.savefig(f"{figTitle}.png")
     plt.close()
     
 def chistplot(filename: str, figTitle: str, figWidth: int, figHeight: int) -> None:
+    """
+    Generated a histogram on the given dataset and saves it.
+    """
     sns.set(color_codes=True)
     df = pd.read_csv(filename)
     df.columns = ["Y_Test", "Y_Preds"]
     fig = plt.figure(figsize=(figWidth, figHeight))
     ax = sns.histplot(df.Y_Test - df.Y_Preds)
     plt.title(f"{figTitle}")
-    plt.savefig(f"chistplot_{figTitle}.png")
+    plt.savefig(f"{figTitle}.png")
     plt.close()
     
