ca593a95ceab3e516d412571cd2331560c708931
jonpsy <gnanasai.n18@iiits.in>
2021-07-06 12:04:17 +0530

    => Change fps to 2 => Set title => Use default color

--- utils/front.py
+++ utils/front.py
@@ -16,20 +16,21 @@ def cfront(nsga2DataX, nsga2DataY, moeadDataX, moeadDataY, filename='fronts.gif'
     iterations = len(nsga2FrontsX)
     count = 0
     
-    with imageio.get_writer(filename, mode='I', fps=1) as writer:
+    with imageio.get_writer(filename, mode='I', fps=2) as writer:
       for i in range(iterations):
         _ , axs = plt.subplots(ncols=2, nrows=1, figsize=(15, 8))
         ## The first axes is for NSGA-II
-        axs[0].scatter(nsga2FrontsX[i], nsga2FrontsY[i], 50, color="blue")
+        axs[0].scatter(nsga2FrontsX[i], nsga2FrontsY[i], 50)
         axs[0].title.set_text("NSGA-II")
         axs[0].set_xlabel("Volatility")
         axs[0].set_ylabel("Returns")
 
         ## The second axes is for MOEAD
-        axs[1].scatter(moeadFrontsX[i], moeadFrontsY[i], 50, color="blue")
+        axs[1].scatter(moeadFrontsX[i], moeadFrontsY[i], 50)
         axs[1].title.set_text("MOEA/D-DE")
         axs[1].set_xlabel("Volatility")
         axs[1].set_ylabel("Returns")
+        plt.suptitle('The Evolution Process via Tracking Pareto Front',fontsize=20)
 
         plt.savefig('c-' + str(count) + '.png')
         plt.close()
