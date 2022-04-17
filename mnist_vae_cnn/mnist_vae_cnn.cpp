a9d62727d11891c8aaefb62b56723558bc569257
Ryan Curtin <ryan@ratml.org>
2022-04-16 20:57:10 -0400

    Include all layer types.

--- mnist_vae_cnn/mnist_vae_cnn.cpp
+++ mnist_vae_cnn/mnist_vae_cnn.cpp
@@ -17,7 +17,7 @@
 #include <mlpack/core.hpp>
 #include <mlpack/core/data/split_data.hpp>
 
-#include <mlpack/methods/ann/layer/layer.hpp>
+#include <mlpack/methods/ann/layer/layer_types.hpp>
 #include <mlpack/methods/ann/ffn.hpp>
 #include <mlpack/methods/ann/init_rules/he_init.hpp>
 #include <mlpack/methods/ann/loss_functions/reconstruction_loss.hpp>
