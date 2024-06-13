d6ae1a61d35647eb11960043585919fb6475c142
Omar Shrit <omar@avontech.fr>
2024-06-13 12:02:22 +0200

    Fix indentation and add pima dataset
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- tools/download_data_set.py
+++ tools/download_data_set.py
@@ -47,126 +47,131 @@ def convert(imgf, labelf, outf, n):
     t.close()
 
 def create_dataset_dir():
-  if os.path.exists("../data"):
-    os.chdir("../data")
-  else:
-    os.mkdir("../data")
-    os.chdir("../data")
+    if os.path.exists("../data"):
+      os.chdir("../data")
+    else:
+      os.mkdir("../data")
+      os.chdir("../data")
 
 def clean():
-  dataset = "."
-  files = os.listdir(dataset)
-  for f in files:
-    if f.endswith(".gz"):
-      os.remove(os.path.join(dataset, f))
-    elif f.endswith(".tar.gz"):
-      os.remove(os.path.join(dataset, f))
-    elif f.endswith(".ubytes"):
-      os.remove(os.path.join(dataset, f))
+    dataset = "."
+    files = os.listdir(dataset)
+    for f in files:
+      if f.endswith(".gz"):
+        os.remove(os.path.join(dataset, f))
+      elif f.endswith(".tar.gz"):
+        os.remove(os.path.join(dataset, f))
+      elif f.endswith(".ubytes"):
+        os.remove(os.path.join(dataset, f))
 
 def ungzip(gzip_file, outputfile):
-  with open(outputfile, 'wb') as f_in, gzip.open(gzip_file, 'rb') as f_out:
-    shutil.copyfileobj(f_out, f_in)
+    with open(outputfile, 'wb') as f_in, gzip.open(gzip_file, 'rb') as f_out:
+      shutil.copyfileobj(f_out, f_in)
 
 def progress_bar(outputfile, request):
-  total_size = int(request.headers.get('content-length', 0))
-  block_size = 1024 #1 Kibibyte
-  t=tqdm(total=total_size, unit='iB', unit_scale=True)
-  with open(outputfile, 'wb') as f:
-    for data in request.iter_content(block_size):
-      t.update(len(data))
-      f.write(data)
-  t.close()
-  if total_size != 0 and t.n != total_size:
-    print("ERROR, something went wrong")
+    total_size = int(request.headers.get('content-length', 0))
+    block_size = 1024 #1 Kibibyte
+    t=tqdm(total=total_size, unit='iB', unit_scale=True)
+    with open(outputfile, 'wb') as f:
+      for data in request.iter_content(block_size):
+        t.update(len(data))
+        f.write(data)
+    t.close()
+    if total_size != 0 and t.n != total_size:
+      print("ERROR, something went wrong")
 
 def mnist_dataset():
-  print("Start downloading the mnist dataset")
-  train_features = requests.get(
-      "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz")
-  progress_bar("train_features.gz", train_features)
-  ungzip("train_features.gz", "train_features.ubytes")
-
-  train_labels = requests.get(
-      "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz")
-  progress_bar("train_labels.gz", train_labels)
-  ungzip("train_labels.gz", "train_labels.ubytes")
-
-  test_features = requests.get(
-      "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz")
-  progress_bar("test_features.gz", test_features)
-  ungzip("test_features.gz", "test_features.ubytes")
-
-  test_labels = requests.get(
-      "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz")
-  progress_bar("test_labels.gz", test_labels)
-  ungzip("test_labels.gz", "test_labels.ubytes")
-
-  print("Converting mnist ubytes images files into csv...")
-  print("This might take a while...")
-  print("Converting features images...")
-  convert("train_features.ubytes", "train_labels.ubytes", "mnist_train.csv", 60000)
-  print("Converting label images...")
-  convert("test_features.ubytes", "test_labels.ubytes", "mnist_test.csv", 10000)
-  clean()
+    print("Start downloading the mnist dataset")
+    train_features = requests.get(
+        "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz")
+    progress_bar("train_features.gz", train_features)
+    ungzip("train_features.gz", "train_features.ubytes")
+  
+    train_labels = requests.get(
+        "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz")
+    progress_bar("train_labels.gz", train_labels)
+    ungzip("train_labels.gz", "train_labels.ubytes")
+  
+    test_features = requests.get(
+        "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz")
+    progress_bar("test_features.gz", test_features)
+    ungzip("test_features.gz", "test_features.ubytes")
+  
+    test_labels = requests.get(
+        "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz")
+    progress_bar("test_labels.gz", test_labels)
+    ungzip("test_labels.gz", "test_labels.ubytes")
+  
+    print("Converting mnist ubytes images files into csv...")
+    print("This might take a while...")
+    print("Converting features images...")
+    convert("train_features.ubytes", "train_labels.ubytes", "mnist_train.csv", 60000)
+    print("Converting label images...")
+    convert("test_features.ubytes", "test_labels.ubytes", "mnist_test.csv", 10000)
+    clean()
 
 def electricity_consumption_dataset():
-  print("Download the electricty consumption example datasets")
-  electricity = requests.get("https://datasets.mlpack.org/examples/electricity-usage.csv")
-  progress_bar("electricity-usage.csv", electricity)
+    print("Download the electricty consumption example datasets")
+    electricity = requests.get("https://datasets.mlpack.org/examples/electricity-usage.csv")
+    progress_bar("electricity-usage.csv", electricity)
 
 def stock_exchange_dataset():
-  print("Download the stock exchange example datasets")
-  stock = requests.get("https://datasets.mlpack.org/examples/Google2016-2019.csv")
-  progress_bar("Google2016-2019.csv", stock)
+    print("Download the stock exchange example datasets")
+    stock = requests.get("https://datasets.mlpack.org/examples/Google2016-2019.csv")
+    progress_bar("Google2016-2019.csv", stock)
 
 def body_fat_dataset():
-  print("Download the body fat datasets")
-  bodyFat = requests.get("https://datasets.mlpack.org/examples/bodyfat.tsv")
-  progress_bar("BodyFat.tsv", bodyFat)
+    print("Download the body fat datasets")
+    bodyFat = requests.get("https://datasets.mlpack.org/examples/bodyfat.tsv")
+    progress_bar("BodyFat.tsv", bodyFat)
 
 def iris_dataset():
-  print("Downloading iris datasets...")
-  iris = requests.get("https://datasets.mlpack.org/iris.tar.gz")
-  progress_bar("iris.tar.gz", iris)
-  tar = tarfile.open("iris.tar.gz", "r:gz")
-  tar.extractall()
-  tar.close()
-  clean()
+    print("Downloading iris datasets...")
+    iris = requests.get("https://datasets.mlpack.org/iris.tar.gz")
+    progress_bar("iris.tar.gz", iris)
+    tar = tarfile.open("iris.tar.gz", "r:gz")
+    tar.extractall()
+    tar.close()
+    clean()
 
 def spam_dataset():
-  print("Downloading spam dataset...")
-  spam = requests.get("https://datasets.mlpack.org/dataset_sms_spam_bhs_indonesia_v1.tar.gz")
-  progress_bar("dataset_sms_spam_bhs_indonesia_v1.tar.gz", spam)
-  tar = tarfile.open("dataset_sms_spam_bhs_indonesia_v1.tar.gz", "r:gz")
-  tar.extractall()
-  tar.close()
-  clean()
+    print("Downloading spam dataset...")
+    spam = requests.get("https://datasets.mlpack.org/dataset_sms_spam_bhs_indonesia_v1.tar.gz")
+    progress_bar("dataset_sms_spam_bhs_indonesia_v1.tar.gz", spam)
+    tar = tarfile.open("dataset_sms_spam_bhs_indonesia_v1.tar.gz", "r:gz")
+    tar.extractall()
+    tar.close()
+    clean()
 
 def salary_dataset():
-  print("Downloading salary dataset...")
-  salary = requests.get("http://datasets.mlpack.org/Salary_Data.csv")
-  progress_bar("Salary_Data.csv", salary)
+    print("Downloading salary dataset...")
+    salary = requests.get("http://datasets.mlpack.org/Salary_Data.csv")
+    progress_bar("Salary_Data.csv", salary)
+
+def pima_diabetes_dataset():
+    print("Downloading pima diabetes dataset...")
+    pima = requests.get("https://datasets.mlpack.org/pima-indians-diabetes.csv")
+    progress_bar("pima-indians-diabetes.csv", pima)
 
 def cifar10_dataset():
-  print("Downloading CIFAR10 dataset...")
-  cifar = requests.get("http://datasets.mlpack.org/cifar-10.tar.xz")
-  progress_bar("cifar-10.tar.xz", cifar)
-  tar = tarfile.open("cifar-10.tar.xz", "r:xz")
-  tar.extractall()
-  tar.close()
-  clean()
+    print("Downloading CIFAR10 dataset...")
+    cifar = requests.get("http://datasets.mlpack.org/cifar-10.tar.xz")
+    progress_bar("cifar-10.tar.xz", cifar)
+    tar = tarfile.open("cifar-10.tar.xz", "r:xz")
+    tar.extractall()
+    tar.close()
+    clean()
 
 def all_datasets():
-  mnist_dataset()
-  electricity_consumption_dataset()
-  stock_exchange_dataset()
-  iris_dataset()
-  salary_dataset()
-  body_fat_dataset()
-  spam_dataset()
-  cifar10_dataset()
-
+    mnist_dataset()
+    electricity_consumption_dataset()
+    stock_exchange_dataset()
+    iris_dataset()
+    salary_dataset()
+    body_fat_dataset()
+    spam_dataset()
+    cifar10_dataset()
+    pima_diabetes_dataset()
 
 if __name__ == '__main__':
 
@@ -191,6 +196,7 @@ if __name__ == '__main__':
         spam : will download the spam dataset
         salary: will download the salary dataset
         cifar10: will download the cifar10 dataset
+        pima: will download the pima diabetes dataset
         all : will download all datasets for all examples
         '''))
 
@@ -226,6 +232,9 @@ if __name__ == '__main__':
       elif args.dataset_name == "cifar10":
         create_dataset_dir()
         cifar10_dataset()
+    elif args.dataset_name == "pima":
+        create_dataset_dir()
+        pima_diabetes_dataset()
       elif args.dataset_name == "all":
         create_dataset_dir()
         all_datasets()
